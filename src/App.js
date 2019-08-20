880b11dd8abc527af40e090a1a526b5820cf40f2
Anubhav Ujjawal <anubhavujjawal@gmail.com>
2019-08-21 00:01:49 +0530

    added a feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Ok? LOL
+          Change from feature branch to be used to create a <code>pull request</code>
         </p>
         <a
           className="App-link"
